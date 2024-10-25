f903455723d57e4f00576f141d4df207368d30b1
Twitchy Twitchy <xziyy1153@gmail.com>
2024-10-25 22:35:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,27 +5,26 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var favicon = require('serve-favicon');
 var createError = require('http-errors');
-var { color } = require('./library/color');
+var { color } = require('./lib/color');
 cors = require('cors'),
 secure = require('ssl-express-www');
 
-//port
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8080 || 5000 || 3000
 
 // Image title
 app.use(favicon(path.join(__dirname,'public','images','favicon.ico')))
 
-// Menyajikan file statis dari folder "public"
-app.use("/static", express.static(path.join(__dirname, "public")));
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 var main = require('./routes/main'),
     api = require('./routes/api')
-app.enable("trust proxy");
-app.set("json spaces", 2);
+
+app.set('trust proxy', true);
+app.set("json spaces",2)
 app.use(cors())
 app.use(secure)
 app.use(cookieParser());
+app.use(express.static("public"))
 app.use('/', main)
 app.use('/api', api)
 
@@ -37,9 +36,11 @@ app.use(function (err, req, res, next) {
 	res.sendFile(__path + '/view/404.html')
   })
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
 // red','green','yellow','blue','magenta','cyan','white']
+app.listen(PORT, () => {
+    console.log(color("<=====[ START HOSTING ]=====>",'red'))
+    console.log(color("Server running on port " + PORT,'white'))
+})
 
 module.exports = app
+
